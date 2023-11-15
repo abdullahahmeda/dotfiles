dab4182c0e79a205ad4d8e227c835cd0a1a89b92
Abdullah <abdullah.ahmed.a2000@gmail.com>
2023-11-15 07:18:16 +0200

    ok

---
+++ lua/custom/plugins/file_browser.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup {
+      extensions = {
+        file_browser = {
+          theme = "dropdown",
+          -- disables netrw and use telescope-file-browser in its place
+          path = "%:p:h",
+          hijack_netrw = true,
+          grouped = true,
+          hidden = true,
+          mappings = {
+            ["i"] = {
+              -- your custom insert mode mappings
+            },
+            ["n"] = {
+              -- your custom normal mode mappings
+            },
+          },
+        },
+      },
+    }
+
+    -- To get telescope-file-browser loaded and working with telescope,
+    -- you need to call load_extension, somewhere after setup function:
+    require("telescope").load_extension "file_browser"
+  end
+}
